13bd4fbc3d41ad9ff862ea5462eea45e8816e982
Pires95 <mpires@alumnos.uvigo.es>
2016-06-22 15:25:04 +0200

    reporter.js templates added

--- examples/quohyperty/hypReporter.js
+++ examples/quohyperty/hypReporter.js
@@ -53,3 +53,29 @@ function sayRan(event) {
     $('.rand-again').on('click', sayRand);
 }
 
+Handlebars.getTemplate = function(name) {
+
+  return new Promise(function(resolve, reject) {
+
+    if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
+      Handlebars.templates = {};
+    } else {
+      resolve(Handlebars.templates[name]);
+    }
+
+    $.ajax({
+      url: 'templates/' + name + '.hbs',
+      success: function(data) {
+        Handlebars.templates[name] = Handlebars.compile(data);
+        resolve(Handlebars.templates[name]);
+      },
+
+      fail: function(reason) {
+        reject(reason);
+      }
+    });
+
+  });
+
+}
+
