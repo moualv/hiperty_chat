e417b1f23b14a9d120ba5eb673b6bb4caef0db66
Mauro Álvarez Martínez <mauroalvarezmartinez@gmail.com>
2016-06-22 15:37:30 +0200

    quickpatch

--- src/quohyperty/hypObserver.hy.js
+++ src/quohyperty/hypObserver.hy.js
@@ -7,23 +7,25 @@ class HypObserver extends EventEmitter {
 
   constructor(hypertyURL, bus, configuration) {
     super();
-    this._objectDescURL = 'hyperty-catalogue://catalogue.' + domain + '/.well-known/dataschema/hypDataSchema';
+    let _this = this;
+    _this._objectDescURL = 'hyperty-catalogue://catalogue.' + domain + '/.well-known/dataschema/hypDataSchema';
 
     let syncher = new Syncher(hypertyURL, bus, configuration);
     syncher.onNotification(function(event) {
       _this._onNotification(event);
     });
 
-    this._syncher = syncher;
+    _this._syncher = syncher;
   }
 
   _onNotification(event) {
-    this.trigger('invitation', event.identity);
+    let _this = this;
+
+    _this.trigger('invitation', event.identity);
 
     event.ack();
 
-    let this = _this;
-    this.syncher.subscribe(this.objectDescURL, event.url).then( function(objObserver) {
+    _this.syncher.subscribe(_this.objectDescURL, event.url).then( function(objObserver) {
       //cambiar el trigger
       _this.trigger('random-number', objObserver.data);
 
