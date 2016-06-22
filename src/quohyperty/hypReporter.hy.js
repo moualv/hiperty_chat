e9007bdfc04bbf8eb47fdb1f5c3dff6746bb687b
Guiamrey <guimarey.r@gmail.com>
2016-06-22 15:27:10 +0200

    reporter done

---
+++ src/quohyperty/hypReporter.hy.js
@@ -0,0 +1,74 @@
+import {Syncher} from 'service-framework/dist/Syncher';
+import {divideURL} from '../utils/utils';
+import hello from './hello';
+
+class HypReporter {
+
+	constructor(hypertyURL, bus, configuration) {
+
+	    if (!hypertyURL) throw new Error('The hypertyURL is a needed parameter');
+	    if (!bus) throw new Error('The MiniBus is a needed parameter');
+	    if (!configuration) throw new Error('The configuration is a needed parameter');
+
+	    let domain = divideURL(hypertyURL).domain;
+    	_this._domain = domain;
+	    this._objectDescURL = 'hyperty-catalogue://catalogue.' + domain + '/.well-known/dataschema/hypDataSchema';
+
+	    let syncher = new Syncher(hypertyURL, bus, configuration);
+
+    	_this._syncher = syncher;
+
+	}
+
+	random(hypertyURL) {
+
+		let syncher = _this._syncher;
+
+	    return new Promise(function(resolve, reject) {
+
+
+	      syncher.create(_this._objectDescURL, [hypertyURL], random).then(function(objReporter) {
+	        console.info('1. Return Created Random Number Data Object Reporter', objReporter);
+
+	        this.objReporter = objReporter;
+	        this.objReporter.data.random = Math.random();
+
+	        objReporter.onSubscription(function(event) {
+	          console.info('-------- Random Number Reporter received subscription request --------- \n');
+
+	          // All subscription requested are accepted
+
+	          event.accept();
+	        });
+
+	        resolve(objReporter);
+
+	      })
+	      .catch(function(reason) {
+	        console.error(reason);
+	        reject(reason);
+	      });
+
+	    });
+  
+
+	}
+
+
+	random-again() {
+
+    	console.log('bye:', this.objReporter );
+
+	    this.objReporter.data.random = Math.random();
+  	}
+
+}
+
+
+export default function activate(hypertyURL, bus, configuration) {
+  return {
+    name: 'hypReporter',
+    instance: new HypReporter(hypertyURL, bus, configuration)
+  };
+
+}
